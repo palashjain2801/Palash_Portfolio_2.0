4897890379685621409313b313053a7b4369843b
palashjain2801 <palashjain2801@gmail.com>
2020-12-26 22:19:51 -0600

    udpte

---
+++ build/precache-manifest.8556d1585d780cdfcbba4bbd598c1cd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7d17d0a0e5e0d75313d16c79413f22",
+    "url": "/Palash_Portfolio_2.0/index.html"
+  },
+  {
+    "revision": "506f98d7f4a9af3ee8bb",
+    "url": "/Palash_Portfolio_2.0/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "a29fbb8e610dc2d33b39",
+    "url": "/Palash_Portfolio_2.0/static/js/2.d593c0f0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Palash_Portfolio_2.0/static/js/2.d593c0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506f98d7f4a9af3ee8bb",
+    "url": "/Palash_Portfolio_2.0/static/js/main.c87aad76.chunk.js"
+  },
+  {
+    "revision": "a75ec9a96d1fd2d955fb",
+    "url": "/Palash_Portfolio_2.0/static/js/runtime-main.9bf4acd9.js"
+  },
+  {
+    "revision": "9f627bc8e6464dfa43f30ca891e3f4c1",
+    "url": "/Palash_Portfolio_2.0/static/media/Palash_Jain.9f627bc8.jpg"
+  },
+  {
+    "revision": "50223ee749928be71502e925e7706689",
+    "url": "/Palash_Portfolio_2.0/static/media/Resume_Palash.50223ee7.pdf"
+  }
+]);
