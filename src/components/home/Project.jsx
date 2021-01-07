c9a07a6ec137e0609496d6431e1689e8cef6089f
palashjain2801 <palashjain2801@gmail.com>
2021-01-07 11:19:47 -0600

    update

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -75,9 +75,9 @@ const Project = ({ heading, username, length, specfic }) => {
         <Card.Body>
           <Card.Title as="h5">Amazon Clone </Card.Title>
           <Card.Text>This is Replica of Amazon Website. Which includes Functionalities like: Create Account, Login, Add Items to bucket, Cart, Payment.</Card.Text>
-          <Button variant="primary" href = " https://github.com/palashjain2801/Amazon-Clone" size="lg" block>Github</Button>
+          <Button variant="primary" href = " https://github.com/palashjain2801/Amazon-Clone" target=" " size="lg" block>Github</Button>
           
-          <Button variant="success" href = " https://palashjain2801.github.io/Amazon-Clone/" size="lg" block>Host</Button>
+          <Button variant="success" href = " https://palashjain2801.github.io/Amazon-Clone/" target=" " size="lg" block>Host</Button>
 
         </Card.Body>
         </Card>
@@ -90,9 +90,9 @@ const Project = ({ heading, username, length, specfic }) => {
         <Card.Body>
           <Card.Title as="h5">Netflix Clone </Card.Title>
           <Card.Text>This is Replica of Netflix Website. Which includes Functionalities of Netflix. You can watch trailer inspite of movies </Card.Text>
-          <Button variant="primary" href = "https://github.com/palashjain2801/Netflix_Clone" size="lg" block>Github</Button>
+          <Button variant="primary" href = "https://github.com/palashjain2801/Netflix_Clone" target=" " size="lg" block>Github</Button>
           
-          <Button variant="success" href = "https://netflix-clone-87dff.firebaseapp.com/" size="lg" block>Host</Button>
+          <Button variant="success" href = "https://netflix-clone-87dff.firebaseapp.com/" target=" " size="lg" block>Host</Button>
 
         </Card.Body>
         </Card>
@@ -107,9 +107,9 @@ const Project = ({ heading, username, length, specfic }) => {
         <Card.Body>
           <Card.Title as="h5">Corona Tracker  </Card.Title>
           <Card.Text>Develop an intuitive and user-friendly website for live tracking of COVID-19 cases in 108 countries. Develop using Redux and ReactJS components to call the Mathdroid API and display the data in a graphical format. Mobile Responsive design the user interface using MaterialUI.</Card.Text>
-          <Button variant="primary" href = "https://github.com/palashjain2801/Corona_website_react" size="lg" block>Github</Button>
+          <Button variant="primary" href = "https://github.com/palashjain2801/Corona_website_react" target=" " size="lg" block>Github</Button>
           
-          <Button variant="success" href = "https://palashjain2801.github.io/Corona_website_react/#/" size="lg" block>Host</Button>
+          <Button variant="success" href = "https://palashjain2801.github.io/Corona_website_react/#/" target=" " size="lg" block>Host</Button>
 
         </Card.Body>
         </Card>
@@ -122,9 +122,9 @@ const Project = ({ heading, username, length, specfic }) => {
         <Card.Body>
           <Card.Title as="h5">Cloud Native Application </Card.Title>
           <Card.Text>Developed Imaging Processing Application and deployed using 3 tire cloud native application architecture using various Amazon Cloud Services such as Auto-Scaling, Cloud based database(Dynamo DB), SNS and Lambda function to process raw image and storing processed image in different S3 Bucket. </Card.Text>
-          <Button variant="primary" href = "https://github.com/palashjain2801/Netflix_Clone" size="lg" block>Github</Button>
+          <Button variant="primary" href = "https://github.com/palashjain2801/Netflix_Clone" target=" " size="lg" block>Github</Button>
           
-          <Button variant="success" href = "https://netflix-clone-87dff.firebaseapp.com/" size="lg" block>Host</Button>
+          <Button variant="success" href = "https://netflix-clone-87dff.firebaseapp.com/" target=" " size="lg" block>Host</Button>
 
         </Card.Body>
         </Card>
