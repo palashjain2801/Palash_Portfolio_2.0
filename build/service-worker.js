d89aa19ced5ded899cdf40258a47021492079eea
palashjain2801 <palashjain2801@gmail.com>
2021-01-08 10:49:49 -0600

    taget blank

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Palash_Portfolio_new/precache-manifest.17a40ec7be88ae67f889713ef77f646c.js"
+  "/Palash_Portfolio_new/precache-manifest.5aad5a3926c647df4e394f67e435e2bb.js"
 );
 
 self.addEventListener('message', (event) => {
